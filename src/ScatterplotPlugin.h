fae25ea0f20cd0c720c44ae044a7f1650dca2716
Julian Thijssen <julianthijssen@gmail.com>
2024-07-23 23:18:32 +0200

    Rename plugin

--- src/ScatterplotPlugin.h
+++ src/ScatterplotPlugin.h
@@ -166,8 +166,8 @@ class ScatterplotPluginFactory : public ViewPluginFactory
 {
     Q_INTERFACES(mv::plugin::ViewPluginFactory mv::plugin::PluginFactory)
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID   "nl.tudelft.ScatterplotPlugin"
-                      FILE  "ScatterplotPlugin.json")
+    Q_PLUGIN_METADATA(IID   "nl.uu.ProjectionExplorer"
+                      FILE  "ProjectionExplorer.json")
     
 public:
     ScatterplotPluginFactory(void) {}
