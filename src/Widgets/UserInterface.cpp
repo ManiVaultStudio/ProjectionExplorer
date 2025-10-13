3500b3678158db80bffc62d86bde93311fb19d8b
Julian Thijssen <julianthijssen@gmail.com>
2025-10-13 17:59:15 +0200

    Turn off unused buttons

--- src/Widgets/UserInterface.cpp
+++ src/Widgets/UserInterface.cpp
@@ -39,8 +39,8 @@ void UserInterface::init()
     initializeDropWidget();
 
     GroupAction* groupAction = new GroupAction(_plugin, "Numeric Group", true);
-    groupAction->addAction(&_settingsAction.getLensRadiusAction(), 3);
-    groupAction->addAction(&_settingsAction.getGlobalRadiusAction(), 3);
+    //groupAction->addAction(&_settingsAction.getLensRadiusAction(), 3);
+    //groupAction->addAction(&_settingsAction.getGlobalRadiusAction(), 3);
     groupAction->addAction(&_settingsAction.getColorOptionAction(), 1);
 
     _primaryToolbarAction.addAction(groupAction, 3, GroupAction::Horizontal);
